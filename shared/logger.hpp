fcb67f67a50a191e08d9ad133e1e7ba5cfb233d4
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-02-23 21:26:00 -0400

    Wacky hacky contexts and file logging

--- shared/logger.hpp
+++ shared/logger.hpp
@@ -48,21 +48,51 @@ namespace Paper {
             throw Exception(fmt::format<TArgs...>(str, std::forward<TArgs>(args)...));
         }
 
+        template<typename Exception = std::runtime_error, typename... TArgs>
+        inline void static fmtThrowError(fmt::format_string<TArgs...> str, fmt::string_view tag, TArgs&&... args) {
+            fmtLog<fmtlog::LogLevel::ERR, TArgs...>(str, tag, std::forward<TArgs>(args)...);
+            throw Exception(fmt::format<std::string_view, TArgs...>("{} " + str, tag, std::forward<TArgs>(args)...));
+        }
+
         template<fmtlog::LogLevel lvl, typename... TArgs>
         constexpr static void fmtLog(fmt::format_string<TArgs...> str, TArgs&&... args) {
-            return vfmtLog<lvl>(str, fmt::make_format_args(args...));
+            return vfmtLog<lvl>(str, "", fmt::make_format_args(args...));
+        }
+
+        template<fmtlog::LogLevel lvl, typename... TArgs>
+        constexpr static void fmtLog(fmt::format_string<TArgs...> str, std::string_view const tag, TArgs&&... args) {
+            return vfmtLog<lvl>(str, tag, fmt::make_format_args(args...));
         }
 
         template<fmtlog::LogLevel level>
-        static void vfmtLog(FmtStringHack const& stringHack, fmt::format_args const& args) noexcept {
+        static void vfmtLog(FmtStringHack const& stringHack, std::string_view const tag, fmt::format_args const& args) noexcept {
             auto const& location = stringHack.loc;
             auto const& str = stringHack.str;
-            FMTLOG(level, "{}.{} {}:{} {}", location.file_name(), location.function_name(), location.line(), location.column(), fmt::vformat(str, args));
+            // fmtlog hack so we can hijack location and use it as a mod id
+
+            do {
+                static uint32_t logId = 0;
+                if (!fmtlog::checkLogLevel(level))break;
+                fmtlogWrapper<>::impl.log(logId, fmtlogT<0>::TSCNS::rdtsc(), tag.data(), level,
+                                          "{}.{} {}:{} {}", location.file_name(), location.function_name(), location.line(),
+                                          location.column(), fmt::vformat(str, args));
+            }
+            while (0);
+
+//            FMTLOG(level, "{}.{} {}:{} {}", location.file_name(), location.function_name(), location.line(), location.column(), fmt::vformat(str, args));
         }
 
-        static std::string_view getLogPathGlobal();
+        static std::string_view getLogDirectoryPathGlobal();
 
         static void Init(std::string_view logPath, std::string_view globalLogFileName = "PaperLog.log");
+
+        static void RegisterContextId(std::string_view contextId, std::string_view logPath);
+
+        static void UnregisterContextId(std::string_view contextId);
+
+        inline static auto RegisterContextId(std::string_view contextId) {
+            return RegisterContextId(contextId, contextId);
+        }
 
     };
 
@@ -71,25 +101,19 @@ namespace Paper {
         using ContextT = ContextType;
 
         ContextT context;
-        std::ofstream fileLog;
 
         constexpr BasicLoggerContext(ContextT const& c) : context(c) {}
-        constexpr BasicLoggerContext(ContextT const& c, std::string_view logName)
-        : context(c), fileLog(fmt::format("{}/{}", Logger::getLogPathGlobal(), logName)) {}
 
 
         template<fmtlog::LogLevel lvl, typename... TArgs>
         constexpr auto fmtLog(fmt::format_string<TArgs...> str, TArgs&&... args) {
-
-
-            return Logger::fmtLog<lvl, decltype(context), TArgs...>("[{}] " + str, context, std::forward<TArgs>(args)...);
-
+            return Logger::fmtLog<lvl, TArgs...>("[{}] " + str, fmt::to_string(context), std::forward<TArgs>(args)...);
 //            Logger::fmtLog<lvl>(fmt::format(FMT_STRING("[{}] {}"), context, fmt::format<TArgs...>(str, std::forward<TArgs>(args)...)));
         }
 
         template<typename Exception = std::runtime_error, typename... TArgs>
         inline auto fmtThrowError(fmt::format_string<TArgs...> str, TArgs&&... args) {
-            return Logger::fmtThrowError<Exception, decltype(context), TArgs...>("[{}] " + str, context, std::forward<TArgs>(args)...);
+            return Logger::fmtThrowError<Exception, TArgs...>("[{}] " + str, fmt::to_string(context), std::forward<TArgs>(args)...);
         }
     };
 
@@ -98,8 +122,12 @@ namespace Paper {
 
 
     namespace Logger {
-        constexpr ModloaderLoggerContext getContext(ModInfo const& info) {
-            return {info};
+        inline ModloaderLoggerContext getModloaderContext(ModInfo const& info) {
+            ModloaderLoggerContext context(info);
+
+            Logger::RegisterContextId(fmt::to_string(info));
+
+            return context;
         }
     }
 
--- src/logger.cpp
+++ src/logger.cpp
@@ -1,13 +1,27 @@
 #include "logger.hpp"
 
+#include <fmt/ostream.h>
+
 #include <optional>
 #include <fstream>
 #include <iostream>
-
 #include <android/log.h>
 #include <thread>
 
+
+struct StringHash {
+    using is_transparent = void; // enables heterogenous lookup
+    std::size_t operator()(std::string_view sv) const {
+        std::hash<std::string_view> hasher;
+        return hasher(sv);
+    }
+};
+
 static std::string globalLogPath;
+using ContextID = std::string;
+using LogPath = std::ofstream;
+
+static std::unordered_map<ContextID, LogPath, StringHash, std::equal_to<>> registeredContexts;
 
 // callback signature user can register
 // ns: nanosecond timestamp
@@ -21,6 +35,12 @@ static std::string globalLogPath;
 void OnLog(int64_t ns, fmtlog::LogLevel level, fmt::string_view location, size_t basePos,
            fmt::string_view threadName, fmt::string_view msg, size_t bodyPos, size_t logFilePos) {
     __android_log_print(level, fmt::format("{}", location).c_str(), "%s", msg.data());
+
+    auto it = registeredContexts.find(location.data());
+    if (it != registeredContexts.end()) {
+        auto& f = it->second;
+        fmt::print(f, FMT_STRING("{}"), msg);
+    }
 }
 
 //void LogThread() {
@@ -33,7 +53,7 @@ void OnLog(int64_t ns, fmtlog::LogLevel level, fmt::string_view location, size_t
 void Paper::Logger::Init(std::string_view logPath, std::string_view globalLogFileName) {
     globalLogPath = logPath;
 
-    fmtlog::setHeaderPattern("{md} [{HMSf}] {l}[{t:<6}]");
+    fmtlog::setHeaderPattern("{md} [{HMSf}] {l}[{t:<6}] {s:<16}:");
     fmtlog::setLogFile(fmt::format("{}/{}", logPath, globalLogFileName).c_str(), false);
     fmtlog::setLogCB(OnLog, fmtlog::LogLevel::DBG);
 
@@ -41,6 +61,15 @@ void Paper::Logger::Init(std::string_view logPath, std::string_view globalLogFil
     fmtlog::startPollingThread();
 }
 
-std::string_view Paper::Logger::getLogPathGlobal() {
+std::string_view Paper::Logger::getLogDirectoryPathGlobal() {
     return globalLogPath;
+}
+
+// TODO: Lock?
+void Paper::Logger::RegisterContextId(std::string_view contextId, std::string_view logPath) {
+    registeredContexts.try_emplace(contextId.data(), fmt::format("{}/{}", getLogDirectoryPathGlobal(), logPath));
+}
+
+void Paper::Logger::UnregisterContextId(std::string_view contextId) {
+    registeredContexts.erase(contextId.data());
 }
