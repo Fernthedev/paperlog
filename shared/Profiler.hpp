80d4502f1bfc0d779c35a1d47318711c1e844c43
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-03-12 15:08:20 -0400

    Add profile benchmark and class

---
+++ shared/Profiler.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <chrono>
+#include <string_view>
+#include <string>
+#include <optional>
+#include <vector>
+
+#include "logger.hpp"
+
+namespace Paper {
+    struct ProfileData {
+        std::chrono::high_resolution_clock::time_point time;
+        std::string log;
+        bool showTime = true;
+
+        ProfileData(const std::chrono::high_resolution_clock::time_point &time, std::string_view log, bool showTime)
+                : time(time), log(log), showTime(showTime) {}
+
+        ProfileData(const std::chrono::high_resolution_clock::time_point &time, std::string_view log) : time(time),
+                                                                                                        log(log) {}
+
+    };
+
+    struct Profiler {
+        inline Profiler() {
+            startTimer();
+        }
+
+        void startTimer() {
+            start = std::chrono::high_resolution_clock::now();
+        }
+
+        void endTimer() {
+            end = std::chrono::high_resolution_clock::now();
+        }
+
+        void mark(std::string_view name, bool log = true) {
+            points.emplace_back(std::chrono::high_resolution_clock::now(), name, log);
+        }
+
+        void mark(ProfileData const &profileData) {
+            points.emplace_back(profileData);
+        }
+
+        void printMarks() const {
+            auto before = start;
+
+            for (auto const &point: points) {
+                auto time = point.time;
+                auto const &name = point.log;
+
+                if (point.showTime) {
+                    auto difference = time - before;
+                    auto millisElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
+                    Logger::fmtLog<LogLevel::DBG>("{} {}ms", name, millisElapsed);
+                    before = time;
+                } else {
+                    Logger::fmtLog<LogLevel::DBG>("{}", name);
+                }
+            }
+
+            auto endMark = end ? end.value() : std::chrono::high_resolution_clock::now();
+
+            auto finishTime = std::chrono::duration_cast<std::chrono::milliseconds>(endMark - start).count();
+
+            Logger::fmtLog<LogLevel::DBG>("Finished! Took {}ms", finishTime);
+        }
+
+        [[nodiscard]] auto elapsedTimeSinceNow() const {
+            return std::chrono::high_resolution_clock::now() - start;
+        }
+
+        [[nodiscard]] auto elapsedTime() const {
+            return end.value() - start;
+        }
+
+
+    private:
+        std::chrono::high_resolution_clock::time_point start;
+        std::optional <std::chrono::high_resolution_clock::time_point> end;
+
+        // pair instead of map to keep order
+        std::vector <ProfileData> points;
+    };
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "modinfo_fmt.hpp"
 #include "logger.hpp"
+#include "Profiler.hpp"
 
 static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
 
@@ -17,11 +18,15 @@ extern "C" void setup(ModInfo& info) {
 // Called later on in the game loading - a good time to install function hooks
 extern "C" void load() {
 
+    Paper::Profiler profiler;
+    profiler.startTimer();
     Paper::Logger::Init("/sdcard/Android/data/com.beatgames.beatsaber/files/logs");
     //        // creates a file logger
 
+    profiler.mark("Init");
 
     Paper::Logger::fmtLog<Paper::LogLevel::INF>("hi! {}", 5);
+    profiler.mark("Sent 1 log");
     Paper::Logger::fmtLog<Paper::LogLevel::INF>("Paper loaded! {}", modInfo);
     Paper::Logger::fmtLog<Paper::LogLevel::INF>("5 \n\n\n\n\nlines", modInfo);
     Paper::Logger::fmtLog<Paper::LogLevel::INF>("Testing weird UTF-8 chars £\n"
@@ -30,9 +35,25 @@ extern "C" void load() {
                                                 "한\n");
 
     Paper::Logger::WaitForFlush();
+    profiler.mark("Flushed");
     Paper::Logger::Backtrace(5);
+    profiler.mark("Backtrace");
 
     auto fastContext = Paper::Logger::WithContext<"PaperFast">();
     fastContext.fmtLog<Paper::LogLevel::INF>("Paper fast!");
+    profiler.printMarks();
 
+
+
+
+    profiler = Paper::Profiler();
+    profiler.startTimer();
+    fastContext.fmtLog<Paper::LogLevel::DBG>("Spam logging now!");
+    for (int i = 0; i < 100000; i++) {
+        fastContext.fmtLog<Paper::LogLevel::DBG>("log i {}", i);
+    }
+    profiler.mark("Logged 100000 times");
+    Paper::Logger::WaitForFlush();
+    profiler.mark("Flushed 100000 logs");
+    profiler.printMarks();
 }
