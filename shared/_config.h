092317c90fb729134bc4108791b073a182d03d81
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2023-05-16 11:48:23 -0400

    Add modloader define to remove

--- shared/_config.h
+++ shared/_config.h
@@ -4,6 +4,11 @@
 #define PAPER_QUEST_MODLOADER
 #endif
 
+#ifdef NO_PAPER_QUEST_MODLOADER
+#undef PAPER_QUEST_MODLOADER
+#warning Removing quest modloader dependency
+#endif
+
 #ifdef PAPER_QUEST_MODLOADER
 #define PAPER_NO_INIT
 // TODO: Add some more potentially specific includes here
