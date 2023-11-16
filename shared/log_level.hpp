022769d564eaa7e0ae7e6fcb8e20675322cab257
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2023-11-16 15:19:01 -0400

    Replace logl level fmt struct with simpler `format_as`

--- shared/log_level.hpp
+++ shared/log_level.hpp
@@ -5,40 +5,30 @@
 
 namespace Paper {
 enum class LogLevel : uint8_t { DBG = 3, INF = 4, WRN = 5, ERR = 6, CRIT = 7, OFF = 0 };
-}
 
-template <> struct fmt::formatter<Paper::LogLevel> : formatter<string_view> {
+constexpr auto format_as(LogLevel level) {
+  switch (level) {
+  case LogLevel::DBG:
+    return "DEBUG";
+    break;
 
-  // Formats the point p using the parsed format specification (presentation)
-  // stored in this formatter.
-  template <typename FormatContext> auto format(Paper::LogLevel level, FormatContext& ctx) -> decltype(ctx.out()) {
-    using namespace Paper;
+  case LogLevel::INF:
+    return "INFO";
+    break;
 
-    std::string_view levelStr = "UNKNOWN";
-    switch (level) {
-    case LogLevel::DBG:
-      levelStr = "DEBUG";
-      break;
+  case LogLevel::WRN:
+    return "WARN";
+    break;
 
-    case LogLevel::INF:
-      levelStr = "INFO";
-      break;
+  case LogLevel::ERR:
+    return "ERROR";
+    break;
 
-    case LogLevel::WRN:
-      levelStr = "WARN";
-      break;
-
-    case LogLevel::ERR:
-      levelStr = "ERROR";
-      break;
-
-    case LogLevel::CRIT:
-      levelStr = "CRITICAL";
-      break;
-    default:
-      break;
-    }
-
-    return formatter<string_view>::format(levelStr, ctx);
+  case LogLevel::CRIT:
+    return "CRITICAL";
+    break;
+  default:
+    return "UNKNOWN";
   }
-};
+}
+} // namespace Paper
