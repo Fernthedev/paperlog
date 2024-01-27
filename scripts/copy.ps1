553bbe06126125c6401d47cb14d717f0ce4fa6f8
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-01-27 18:40:22 -0400

    Fix copy dest

--- scripts/copy.ps1
+++ scripts/copy.ps1
@@ -52,7 +52,7 @@ if ($useDebug -eq $true) {
     $fileName = Get-ChildItem lib*.so -Path "build/" -Name
 }
 
-& adb push build/$fileName /sdcard/Android/data/com.beatgames.beatsaber/files/libs/$fileName
+& adb push build/$fileName /sdcard/ModData/com.beatgames.beatsaber/Modloader/libs/$fileName
 
 & $PSScriptRoot/restart-game.ps1
 
