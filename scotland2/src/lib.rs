867764fedc1a3b780c9990df4ab4bbd7cee3bad0
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-12-14 21:19:18 -0400

    Fix path

--- scotland2/src/lib.rs
+++ scotland2/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ffi::CStr;
+use std::{ffi::CStr, path::PathBuf};
 
 use ctor::ctor;
 use paper2::LoggerConfig;
@@ -10,9 +10,14 @@ fn dlopen_initialize() {
     println!("DLOpen initializing");
 
     let id = unsafe { CStr::from_ptr(scotland2::modloader_get_application_id()) }.to_string_lossy();
-    let path = format!("/sdcard/ModData/{id}/logs2",);
+    let path = PathBuf::from(format!("/sdcard/ModData/{id}/logs2",));
+    let config = LoggerConfig {
+        context_log_path: path.clone(),
+        ..LoggerConfig::default()
+    };
 
-    if let Err(e) = paper2::init_logger(LoggerConfig::default(), path.into()) {
+    let file = path.join("Paperlog.log");
+    if let Err(e) = paper2::init_logger(config, file) {
         eprintln!("Error occurred in logging thread: {}", e);
         panic!("Error occurred in logging thread: {}", e);
     }
