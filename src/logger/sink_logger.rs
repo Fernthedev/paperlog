c46b8bf4fe5961ca9ce7b1980d1ccbc5de287fc0
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2025-02-22 19:20:44 -0400

    Fix arc ref in sink

--- src/logger/sink_logger.rs
+++ src/logger/sink_logger.rs
@@ -4,7 +4,7 @@ use super::log_data::LogData;
 
 pub(crate) fn do_log(
     log: &LogData,
-    logger_thread: std::sync::Arc<std::sync::RwLock<super::LoggerThread>>,
+    logger_thread: &std::sync::RwLock<super::LoggerThread>,
 ) -> Result<()> {
     let sinks = &logger_thread.read().unwrap().sinks;
 
