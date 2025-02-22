914a5651deeec76313402279aee78fe2a1de034e
Alex <FrozenAlex@users.noreply.github.com>
2025-02-23 00:05:41 +0400

    Fix quest 1 logging and make panic hooks log to logcat (#13)
    * Fix quest 1 logging and make panic hooks log to logcat
    
    * Fix api30 buffer
    
    * Add the actual panic hook
    
    * Format code and remove tracing-error

--- scotland2/src/lib.rs
+++ scotland2/src/lib.rs
@@ -1,7 +1,8 @@
-use std::{ffi::CStr, path::PathBuf};
-
 use ctor::ctor;
 use paper2::LoggerConfig;
+use std::backtrace::Backtrace;
+use std::panic::PanicHookInfo;
+use std::{ffi::CStr, path::PathBuf,ffi::CString};
 
 use mimalloc::MiMalloc;
 
@@ -11,7 +12,8 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 #[ctor]
 fn dlopen_initialize() {
-    println!("DLOpen initializing");
+    log_info(String::from("DLOpen initializing"));
+    std::panic::set_hook(panic_hook(true));
 
     let id = unsafe { CStr::from_ptr(scotland2_rs::scotland2_raw::modloader_get_application_id()) }
         .to_string_lossy();
@@ -23,7 +25,65 @@ fn dlopen_initialize() {
 
     let file = path.join("Paperlog.log");
     if let Err(e) = paper2::init_logger(config, file) {
-        eprintln!("Error occurred in logging thread: {}", e);
+        log_info(String::from(format!(
+            "Error occurred in logging thread: {}",
+            e
+        )));
         panic!("Error occurred in logging thread: {}", e);
     }
+}
+
+fn log_error(message_str: String) {
+    use ndk_sys::{__android_log_write, android_LogPriority};
+
+    let priority = android_LogPriority::ANDROID_LOG_ERROR.0;
+    let tag = CString::from(c"E");
+    let msg = match CString::new(message_str) {
+        Ok(s) => s,
+        Err(_e) => {
+            return;
+        }
+    };
+
+    unsafe { __android_log_write(priority as i32, tag.as_ptr(), msg.as_ptr()) };
+}
+
+fn log_info(message_str: String) {
+    use ndk_sys::{__android_log_write, android_LogPriority};
+
+    let priority = android_LogPriority::ANDROID_LOG_INFO.0;
+    let tag = CString::from(c"I");
+    let msg = match CString::new(message_str) {
+        Ok(s) => s,
+        Err(_e) => {
+            return;
+        }
+    };
+
+    unsafe { __android_log_write(priority as i32, tag.as_ptr(), msg.as_ptr()) };
+}
+
+pub fn panic_hook(backtrace: bool) -> Box<dyn Fn(&PanicHookInfo<'_>) + Send + Sync + 'static> {
+    // Mostly taken from https://doc.rust-lang.org/src/std/panicking.rs.html
+    Box::new(move |info| {
+        let location = info.location().unwrap();
+        let msg = match info.payload().downcast_ref::<&'static str>() {
+            Some(s) => *s,
+            None => match info.payload().downcast_ref::<String>() {
+                Some(s) => &s[..],
+                None => "Box<dyn Any>",
+            },
+        };
+
+        log_error(format!(
+            "Panicked at '{}', {} {} {}",
+            location.file(),
+            location.line(),
+            location.column(),
+            msg
+        ));
+        if backtrace {
+            log_error(format!("Backtrace: {:#?}", Backtrace::force_capture()));
+        }
+    })
 }
--- src/logger/logcat_logger.rs
+++ src/logger/logcat_logger.rs
@@ -111,7 +111,7 @@ pub(crate) fn do_log(log: &super::log_data::LogData) -> Result<()> {
     
             let mut message = __android_log_message {
                 struct_size: size_of::<__android_log_message>(),
-                buffer_id: Buffer::Default as i32,
+                buffer_id: Buffer::Main as i32,
                 priority: priority as i32,
                 tag: tag.as_ptr(),
                 file: file.as_ptr(),
@@ -124,9 +124,9 @@ pub(crate) fn do_log(log: &super::log_data::LogData) -> Result<()> {
     
         #[cfg(not(feature = "android-api-30"))]
         {
-            use ndk_sys::__android_log_buf_write;
+            use ndk_sys::__android_log_write;
     
-            unsafe { __android_log_buf_write(Buffer::Default as i32, priority as i32, tag.as_ptr(), message.as_ptr()) };
+            unsafe { __android_log_write(priority as i32, tag.as_ptr(), message.as_ptr()) };
         }
 
         buffer.clear();
