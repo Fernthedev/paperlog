889efd6fbbed6a429b8b4fd53edc3715aacc7f30
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2025-12-05 09:19:02 -0400

    Improve FFI crate

--- paper2_ffi/src/lib.rs
+++ paper2_ffi/src/lib.rs
@@ -1,5 +1,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(clippy::useless_transmute)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::ptr_offset_with_cast)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
